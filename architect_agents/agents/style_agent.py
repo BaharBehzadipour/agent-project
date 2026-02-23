ee8dcb052ec770f66e0f2c5a1816fe3dedd0ac5c
BaharBehzadipour <65755248+BaharBehzadipour@users.noreply.github.com>
2026-02-23 14:17:24 +0330

    Update style_agent.py

--- architect_agents/agents/style_agent.py
+++ architect_agents/agents/style_agent.py
@@ -1,9 +1,49 @@
-class StyleAgent:
-    def act(self, metadata: dict) -> str:
-        style = metadata.get("style", "")
-        if not style:
-            return ""
-        return (
-            f"Architectural style: {style}. "
-            f"Preserve characteristic forms, ornaments, façade language, and proportions of {style} architecture."
-        )
+# class StyleAgent:
+#     def act(self, metadata: dict) -> str:
+#         style = metadata.get("style", "")
+#         if not style:
+#             return ""
+#         return (
+#             f"Architectural style: {style}. "
+#             f"Preserve characteristic forms, ornaments, façade language, and proportions of {style} architecture."
+
+#         )
+
+
+
+
+import torch
+from PIL import Image
+from diffusers import StableDiffusionControlNetPipeline, ControlNetModel
+
+class ControlNetExecutor:
+    def __init__(
+        self,
+        controlnet_path="/content/drive/MyDrive/checkpoints/checkpoints/merged_checkpoint",
+        base_model="runwayml/stable-diffusion-v1-5"
+    ):
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.dtype = torch.float16 if self.device == "cuda" else torch.float32
+        
+        self.controlnet = ControlNetModel.from_pretrained(controlnet_path,
+                                                         torch_dtype=self.dtype).to(self.device)
+
+        self.pipe = StableDiffusionControlNetPipeline.from_pretrained(
+            base_model,
+            controlnet=self.controlnet,
+            torch_dtype=self.dtype
+        ).to(self.device)
+
+    def generate(self,conditioning_path: str, prompt: str):
+        cond = Image.open(conditioning_path).convert("RGB")
+
+        image = self.pipe(
+            prompt=prompt,
+            image=cond,
+            num_inference_steps=30
+            # guidance_scale=7.5
+        ).images[0]
+
+
+        return image
+
